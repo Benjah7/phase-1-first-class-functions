63614844b5d2010e1f8e4a2b40fe19cefe948efa
benja7 <benjaminmutava@gmail.com>
2022-08-23 08:41:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-function receivesAFunction (callback){
-    return callback();
-}
-receivesAFunction (function() { return 'Moringa school' });
-
-function returnsANamedFunction() {
-   return returnsANamedFunction;
-}
-returnsANamedFunction( function() {return 'nothing'});
-
-let returnsAnAnonymousFunction = function(){
-    
-   return function(){'Hello'};
-}
-returnsAnAnonymousFunction();
+const receivesAFunction = (spy) => {
+   spy();
+ };
+ 
+ const returnsANamedFunction = () => {
+   return function receivesAFunction(name) {};
+ };
+ 
+ function returnsAnAnonymousFunction() {
+   return function (name) {
+     // Do something Maureen :)
+   };
+ }
